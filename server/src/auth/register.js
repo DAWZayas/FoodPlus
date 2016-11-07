aaae04bd294abc2396225fade13dcc0ad30482d4
Rowleen <xx_alternativeboy_xx@hotmail.com>
2016-11-07 13:19:59 +0100

    Fix passport.js local setup, use correct login field
    Y más vainitas

---
+++ server/src/auth/register.js
@@ -0,0 +1,33 @@
+// our packages
+import {User} from '../db';
+import {hash, asyncRequest} from '../util';
+
+export default (app) => {
+  app.post('/api/register', asyncRequest(async (req, res) => {
+    // get user input
+    const {login, password, passwordRepeat} = req.body;
+
+    if (password !== passwordRepeat) {
+      res.status(400).send({error: 'Passwords do not match!'});
+      return;
+    }
+    // hash password
+    const hashedPassword = hash(password);
+
+    // check if login already taken
+    const users = await User.filter({login}).run();
+    if (users.length > 0) {
+      res.status(403).send({error: 'User already exists!'});
+      return;
+    }
+
+    // save new user
+    const user = new User({
+      login,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    res.sendStatus(201);
+  }));
+};
