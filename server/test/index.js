21950be2bb6c4bfe96d86d726afb4478f973716e
deividko <david94.correa@gmail.com>
2016-11-09 09:01:46 +0100

    Fix reqlite spawning and killing during testing

--- server/test/index.js
+++ server/test/index.js
@@ -1,21 +1,13 @@
-// npm packages
-import test from 'tape';
-import request from 'supertest';
-
-// our packages
-import app from '../src/app';
-
-test('GET /', (t) => {
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Content-Type', /text\/html/)
-    .end((err, res) => {
-      const expectedBody = 'Hello world!';
-      const actualBody = res.text;
-
-      t.error(err, 'No error');
-      t.equal(actualBody, expectedBody, 'Retrieve body');
-      t.end();
-    });
+/* eslint global-require: 0 */
+// get spawn
+const spawn = require('child_process').spawn;
+// require babel require hook
+require('babel-core/register');
+// reqlite instance
+const reqlite = spawn('reqlite');
+// wait for start
+reqlite.stderr.on('data', () => {
+  // require main tests
+  const startTests = require('./main').default;
+  startTests(reqlite);
 });
