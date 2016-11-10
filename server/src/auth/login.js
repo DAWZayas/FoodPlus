23b945637415b65c3e228a320b2999a91a702090
Rowleen <xx_alternativeboy_xx@hotmail.com>
2016-11-10 14:19:32 +0100

    Add user update route and tests for it && added schema 'Plate'

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -1,13 +1,9 @@
 import passport from 'passport';
-import jwt from 'jsonwebtoken';
-// our packages
-import {auth as authConfig} from '../../config';
 
 export default (app) => {
   app.post('/api/login', passport.authenticate('local'), (req, res) => {
     if (req.user) {
-      const token = jwt.sign(req.user, authConfig.jwtSecret);
-      res.send({user: req.user, token});
+      res.send({user: req.user});
     } else {
       res.status(401).send({error: 'Error logging in!'});
     }
