2c6a2151990f5c38bfd5aacf081e1910e23017d8
Rowleen <xx_alternativeboy_xx@hotmail.com>
2016-11-13 21:09:14 +0100

    add more requeriments to register an user

--- server/src/db/user.js
+++ server/src/db/user.js
@@ -2,6 +2,11 @@ import {thinky} from './thinky';
 
 export const User = thinky.createModel('User', {
   login: thinky.type.string().required(),
+  name: thinky.type.string().required(),
+  surname: thinky.type.string().required(),
+  address: thinky.type.string().required(),
+  telephone: thinky.type.number().required(),
+  email: thinky.type.string().required(),
   password: thinky.type.string().required(),
   registrationDate: thinky.type.date().default(thinky.r.now()),
 });
