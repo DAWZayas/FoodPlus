dabd92b40729d8c150f8584ec02644a5b56ce853
deividko <david94.correa@gmail.com>
2016-11-28 20:39:19 +0100

    Refactor epics into new separate folder

--- client/src/store/rootEpic.js
+++ client/src/store/rootEpic.js
@@ -1,18 +1,4 @@
 import {combineEpics} from 'redux-observable';
-import {Observable} from 'rxjs/Observable';
-import * as ActionTypes from './actionTypes';
+import epics from './epics';
 
-const heloWorld = action$ => action$
-  .ofType(ActionTypes.HELLO_WORLD)
-  .switchMap(()=> Observable
-    .timer(500)
-    .map(()=>({
-      type: ActionTypes.HELLO_WORLD_END,
-      payload:{
-        world: 'world',
-      },
-    }))
-  );
-export default combineEpics(
-  helloWorld,
-);
+export default combineEpics(...epics);
