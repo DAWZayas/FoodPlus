470ab551d131c437920deb02416c1c9366702ad4
Rowleen <xx_alternativeboy_xx@hotmail.com>
2016-12-01 13:51:14 +0100

    [Back] Add question update route and tests

--- server/test/login.js
+++ server/test/login.js
@@ -1,6 +1,7 @@
 // npm packages
 import request from 'supertest';
 import jwt from 'jsonwebtoken';
+
 // our packages
 import app from '../src/app';
 import {auth as authConfig} from '../config';
@@ -18,13 +19,44 @@ export default (test) => {
         t.error(err, 'No error');
         t.ok(actualBody.user, 'User exists');
         t.ok(actualBody.token, 'Token exists');
-          const decodedUser = jwt.verify(actualBody.token, authConfig.jwtSecret);
-          delete decodedUser.iat;
+
+        const decodedUser = jwt.verify(actualBody.token, authConfig.jwtSecret);
+        delete decodedUser.iat;
+
         t.equal(actualBody.user.login, 'test', 'Login matches request');
         t.notOk(actualBody.user.password, 'No password included');
         t.deepEqual(actualBody.user, decodedUser, 'User must match token');
+
         app.set('token', actualBody.token);
         app.set('user', actualBody.user);
+
+        t.end();
+      });
+  });
+
+  test('Should login with other existing username and password', (t) => {
+    request(app)
+      .post('/api/login')
+      .send({login: 'other', password: '321'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.ok(actualBody.user, 'User exists');
+        t.ok(actualBody.token, 'Token exists');
+
+        const decodedUser = jwt.verify(actualBody.token, authConfig.jwtSecret);
+        delete decodedUser.iat;
+
+        t.equal(actualBody.user.login, 'other', 'Login matches request');
+        t.notOk(actualBody.user.password, 'No password included');
+        t.deepEqual(actualBody.user, decodedUser, 'User must match token');
+
+        app.set('other-token', actualBody.token);
+        app.set('other-user', actualBody.user);
+
         t.end();
       });
   });
