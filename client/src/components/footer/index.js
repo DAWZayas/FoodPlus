c339d58b04cef33dbb6cb5468b58a889d2ca8c5f
Julián Peñaranda <xx_alternativeboy_xx@hotmail.com>
2017-01-18 19:53:00 +0100

    [Client] Notifications now have fixed position

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,40 +1,55 @@
-import React from 'react';
+  // render() {
+  //
+  //   return (
+  //     <div className="footer">
+  //     <div className="row">
+  //         <div className="col-md-4">
+  //           <center>
+  //             <h4 className="footertext">Contacto:</h4>
+  //             <ul>
+  //               <li>Calle: la que sea nº11.</li>
+  //               <li>Teléfono: 918988989</li>
+  //               <li>E-mail: foodplus/'@'/gmail.com</li>
+  //            </ul>>
+  //           </center>
+  //         </div>
+  //         <div className="col-md-4">
+  //           <center>
+  //             <h4 className="footertext">SocialNetworks</h4>
+  //               <img src="http://i39.tinypic.com/33o1c44.jpg" />
+  //               <right><img src="http://i43.tinypic.com/2ppijj4.jpg" /></right>
+  //               <left><img src="http://i39.tinypic.com/16bhq41.jpg" /></left>
+  //             </center>
+  //         </div>
+  //         <div className="col-md-4">
+  //           <center>
+  //             <h4 className="footertext">Other</h4>
+  //             <p className="footertext">Other things there<br/>
+  //           </p></center>
+  //         </div>
+  //       </div>
+  //
+  //
+  //
+  //     </div>
+  // npm packages
+  import React from 'react';
 
-export default class Footer extends React.Component {
-  render() {
+  import {Notifications} from '../notifications';
 
-    return (
-      <div className="footer">
-      <div className="row">
-          <div className="col-md-4">
-            <center>
-              <h4 className="footertext">Contacto:</h4>
-              <ul>
-                <li>Calle: la que sea nº11.</li>
-                <li>Teléfono: 918988989</li>
-                <li>E-mail: foodplus/'@'/gmail.com</li>
-             </ul>>
-            </center>
-          </div>
-          <div className="col-md-4">
-            <center>
-              <h4 className="footertext">SocialNetworks</h4>
-                <img src="http://i39.tinypic.com/33o1c44.jpg" />
-                <right><img src="http://i43.tinypic.com/2ppijj4.jpg" /></right>
-                <left><img src="http://i39.tinypic.com/16bhq41.jpg" /></left>
-              </center>
-          </div>
-          <div className="col-md-4">
-            <center>
-              <h4 className="footertext">Other</h4>
-              <p className="footertext">Other things there<br/>
-            </p></center>
-          </div>
-        </div>
+  const style = {
+    footer: {
+      zIndex: 9999,
+      position: 'fixed',
+      bottom: '0',
+      width: '90%',
+    },
+  };
 
-
-
+  export default () => (
+    <footer style={style.footer}>
+      <div className="col-md-8">
+        <Notifications />
       </div>
-    );
-  }
-};
+    </footer>
+  );
