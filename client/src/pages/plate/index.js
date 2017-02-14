634f2f8bf04439cb479411d273aa6fd958ac7bcb
Rowleen <xx_alternativeboy_xx@hotmail.com>
2017-02-14 13:36:20 +0100

    [Client] Now we can get all plates in the same page

--- client/src/pages/plate/index.js
+++ client/src/pages/plate/index.js
@@ -4,7 +4,6 @@ import {connect} from 'react-redux';
 
 // our packages
 import {getAllPlates} from '../../store/actions';
-import {Plates} from '../../components/plate';
 
 const mapStateToProps = state => ({
   plates: state.plates.plates,
@@ -23,10 +22,19 @@ class Plate extends React.Component {
   render() {
     // ¿De quién está cogiendo estas propiedades?
     const {plates} = this.props;
-
     return (
-      <div>
-        <Plates plates={plates} />
+      <div className="row">
+        {plates.map(plate => (
+          <div key={plate.id} className="col-sm-4">
+            <div className="thumbnail">
+              <img src={plate.urlimage} alt={plate.name} />
+              <div className="caption">
+                <h3>{plate.name}</h3>
+                <p>{plate.ingredients}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
