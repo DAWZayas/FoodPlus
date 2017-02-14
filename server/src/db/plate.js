a4eaf7b519604b40db596684be671b025718126e
Rowleen <xx_alternativeboy_xx@hotmail.com>
2017-02-14 12:54:25 +0100

    [Back] added field for url image for plates

--- server/src/db/plate.js
+++ server/src/db/plate.js
@@ -2,5 +2,6 @@ import {thinky} from './thinky';
 
 export const Plate = thinky.createModel('Plate', {
   name: thinky.type.string().required(),
+  urlimage: thinky.type.string().required(),
   ingredients: thinky.type.string().required(),
 });
