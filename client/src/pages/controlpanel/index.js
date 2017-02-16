4269347f2ac41ceba49394688b8caec586e4446e
Julián Peñaranda <xx_alternativeboy_xx@hotmail.com>
2017-02-16 21:43:49 +0100

    [Client] Added welcome message to user and display user on panel profile

--- client/src/pages/controlpanel/index.js
+++ client/src/pages/controlpanel/index.js
@@ -3,48 +3,43 @@ import {Link} from 'react-router';
 import {connect} from 'react-redux';
 
 const mapStateToProps = state => ({
-  user: state.auth;
+  user: state.auth.user,
 });
 
-class ControlPanel extends Component {
-  constructor(props) {
-    super(props);
-  }
+const mapDispatchToProps = dispatch => ({
 
-  render(){
-    const {user} = this.props;
-    return (
-      <section className="row">
-        <div className="col-md-12">
-          <div className="panel panel-primary">
-            <div className="panel-heading">
-              <h3 className="panel-title">Your profile {user.name}</h3>
-            </div>
-            <div className="panel-body">
-              <div className="btn-group btn-group-lg" role="group" aria-label="...">
-                <button type="button" className="btn btn-default"><Link to="/createplate">Create</Link></button>
-                <button type="button" className="btn btn-default"><Link to="/updateplate">Update</Link></button>
-                <button type="button" className="btn btn-default"><Link to="/deleteplate">Delete</Link></button>
-              </div>
-            </div>
+});
+
+const ControlPanel = ({user}) => (
+  <section className="row">
+    <div className="col-md-12">
+      <div className="panel panel-primary">
+        <div className="panel-heading">
+          <h3 className="panel-title">Your profile {user.login}</h3>
+        </div>
+        <div className="panel-body">
+          <div className="btn-group btn-group-lg" role="group" aria-label="...">
+            <button type="button" className="btn btn-default"><Link to="/createplate">Create</Link></button>
+            <button type="button" className="btn btn-default"><Link to="/updateplate">Update</Link></button>
+            <button type="button" className="btn btn-default"><Link to="/deleteplate">Delete</Link></button>
           </div>
         </div>
-        <div className="col-md-12">
-          <div className="panel panel-primary">
-            <div className="panel-heading">
-              <h3 className="panel-title">Manage Plates</h3>
-            </div>
-            <div className="panel-body">
-              <div className="btn-group btn-group-lg" role="group" aria-label="...">
-                <button type="button" className="btn btn-default"><Link to="/createplate">Create</Link></button>
-                <button type="button" className="btn btn-default"><Link to="/updateplate">Update</Link></button>
-                <button type="button" className="btn btn-default"><Link to="/deleteplate">Delete</Link></button>
-              </div>
-            </div>
+      </div>
+    </div>
+    <div className="col-md-12">
+      <div className="panel panel-primary">
+        <div className="panel-heading">
+          <h3 className="panel-title">Manage Plates</h3>
+        </div>
+        <div className="panel-body">
+          <div className="btn-group btn-group-lg" role="group" aria-label="...">
+            <button type="button" className="btn btn-default"><Link to="/createplate">Create</Link></button>
+            <button type="button" className="btn btn-default"><Link to="/updateplate">Update</Link></button>
+            <button type="button" className="btn btn-default"><Link to="/deleteplate">Delete</Link></button>
           </div>
         </div>
-      </section>
-    )
-  }
-};
+      </div>
+    </div>
+  </section>
+)
 export default connect(mapStateToProps, mapDispatchToProps)(ControlPanel);
