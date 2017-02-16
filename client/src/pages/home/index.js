5c189623a1c47f8fbee4f935807648ea278e1f63
Rowleen <xx_alternativeboy_xx@hotmail.com>
2017-02-16 12:58:32 +0100

    [Client] Edited style classes from home

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -2,24 +2,22 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-import style from '../../css/style.css';
-
 const Home = () => (
   <div className="row">
     <div className="col-md-12">
-      <div className={style.deliver}>
-        <Link to="/delivery" className={style.transparentButtons}>Delivery</Link>
-        <Link to="/reservation" className={style.transparentButtons}>Reservation</Link>
+      <div className="deliver">
+        <Link to="/delivery" className="transparent-button">Delivery</Link>
+        <Link to="/reservation" className="transparent-button">Reservation</Link>
       </div>
-      <div className={style.testimonial}>
-        <div className={style.caption}>
-          <h3><Link className={style.homeTitle} to="/testimonials">Testimonial</Link></h3>
+      <div className="testimonial">
+        <div className="caption">
+          <h3><Link className="home-title" to="/testimonials">Testimonial</Link></h3>
           <p>What our customer think about of us</p>
         </div>
       </div>
-      <div className={style.reservation}>
-        <div className={style.caption}>
-          <h3><Link className={style.homeTitle} to="/reservation">Book a table</Link></h3>
+      <div className="reservation">
+        <div className="caption">
+          <h3><Link className="home-title "to="/reservation">Book a table</Link></h3>
           <p>Choose the best table to eat at any time you want</p>
         </div>
       </div>
